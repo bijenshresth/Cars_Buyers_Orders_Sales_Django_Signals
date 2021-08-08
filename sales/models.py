2cd56a3d8b9eb7c278bb5784b884dc3d9a1608ef
bijenshresth <018bscit007@sxc.edu.np>
2021-08-08 12:45:42 +0000

    initial commit

---
+++ sales/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from orders.models import Order
+# Create your models here.
+class Sale(models.Model):
+    order = models.ForeignKey(Order, on_delete = models.CASCADE)
+    amount = models.PositiveIntegerField(blank = True, null = True)
+
+    def __str__(self):
+        return f"{self.order}-{self.amount}"
